e6f0c98eaf4d6db7daac5b679ad89f708db9379f
Michael Kleemoff <96216513+MichaelKleemoff@users.noreply.github.com>
2022-06-20 21:48:18 -0400

    Delete src/Components directory

--- src/Components/VideoDetail.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import Comments from "./Comments/Comments";
-const VideoDetail = () => {
-  return <div className=''>VideoDetail <Comments /></div>;
-};
-
-export default VideoDetail;
